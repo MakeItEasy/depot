bf128fbffad2263358c0ada52f2741b0f706b1a8
dairg <dairg@dairg-virtual-machine.(none)>
2012-09-05 13:29:04 +0800

    Init the app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'f1109af49e5289e4745a6921657aa2cb8a1e6fff428698b127f1034b85f706a79972610a494c4d8c6aa8322f504deb7b58093223f8f5e52d2ad8457897ad364d'
